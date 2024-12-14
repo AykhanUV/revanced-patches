31653fb8ff0e68c413fe73ba99cd131096d43b27
Aykhan <ayxanshikhaliyevuv@gmail.com>
2024-12-14 23:47:33 +0400

    refactor: Bump ReVanced Patcher & merge integrations

--- extensions/shared/src/main/java/app/revanced/extension/shared/patches/PatchStatus.java
+++
@@ -1,8 +0,0 @@
-package app.revanced.extension.shared.patches;
-
-@SuppressWarnings("unused")
-public class PatchStatus {
-    public static boolean HideFullscreenAdsDefaultBoolean() {
-        return false;
-    }
-}
